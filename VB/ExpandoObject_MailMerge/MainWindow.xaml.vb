ebe972ceecc6eab30df4ff64e92c50f6fbce0364
DevExpressExampleBot <fakeEmail>
2021-09-07 19:14:58 +0300

    Source auto update [skip ci]

--- VB/ExpandoObject_MailMerge/MainWindow.xaml.vb
+++ VB/ExpandoObject_MailMerge/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Dynamic
 Imports System.Linq
@@ -12,17 +11,18 @@ Namespace ExpandoObject_MailMerge
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
-			AddHandler Loaded, AddressOf MainWindow_Loaded
+			AddHandler Me.Loaded, AddressOf MainWindow_Loaded
 
 		End Sub
 
 		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
 			richEditControl1.ApplyTemplate()
 
-
-            Dim weathers As Object = GetExpandoFromXml("weather.xml")
+'INSTANT VB NOTE: In the following line, Instant VB substituted 'Object' for 'dynamic' - this will work in VB with Option Strict Off:
+			Dim weathers As Object = GetExpandoFromXml("weather.xml")
 			richEditControl1.Options.MailMerge.DataSource = weathers
 
 			richEditControl1.LoadDocument("weather_report.rtf")
@@ -31,31 +31,33 @@ Namespace ExpandoObject_MailMerge
 			richEditControl1.Options.MailMerge.ViewMergedData = True
 		End Sub
 
-        Public Shared Function GetExpandoFromXml(ByVal file As String) As IList(Of Object)
-            Dim weathers = New List(Of Object)()
+'INSTANT VB NOTE: In the following line, Instant VB substituted 'Object' for 'dynamic' - this will work in VB with Option Strict Off:
+		Public Shared Function GetExpandoFromXml(ByVal file As String) As IList(Of Object)
+'INSTANT VB NOTE: In the following line, Instant VB substituted 'Object' for 'dynamic' - this will work in VB with Option Strict Off:
+			Dim weathers = New List(Of Object)()
 
-            Dim doc = XDocument.Load(file)
-            Dim nodes = _
-             From node In doc.Root.Descendants("weather") _
-             Select node
-            For Each n In nodes
-                Dim MyData As Object = New ExpandoObject()
-                MyData.LastUpdateTime = String.Format("{0:o}", DateTime.Now)
-                MyData.Weather = New ExpandoObject()
-                For Each child In n.Descendants()
+			Dim doc = XDocument.Load(file)
+			Dim nodes = From node In doc.Root.Descendants("weather")
+				Select node
+			For Each n In nodes
+'INSTANT VB NOTE: In the following line, Instant VB substituted 'Object' for 'dynamic' - this will work in VB with Option Strict Off:
+				Dim MyData As Object = New ExpandoObject()
+				MyData.LastUpdateTime = String.Format("{0:o}", DateTime.Now)
+				MyData.Weather = New ExpandoObject()
+				For Each child In n.Descendants()
 
-                    Dim w = TryCast(MyData.Weather, IDictionary(Of String, Object))
-                    Dim atb As XAttribute = child.Attribute("data")
-                    If atb IsNot Nothing Then
-                        w(child.Name.LocalName) = atb.Value
-                    End If
-                Next child
+					Dim w = TryCast(MyData.Weather, IDictionary(Of String, Object))
+					Dim atb As XAttribute = child.Attribute("data")
+					If atb IsNot Nothing Then
+						w(child.Name.LocalName) = atb.Value
+					End If
+				Next child
 
-                weathers.Add(MyData)
+				weathers.Add(MyData)
 
-            Next n
-            Return weathers
-        End Function
+			Next n
+			Return weathers
+		End Function
 
 	End Class
 End Namespace
